e924c311c5dc2abe2fdb16892b16229692d080e2
erina-ameen <erina.ameen@gmail.com>
2025-03-04 14:35:55 +0000

    rocket.py

--- rocket/rocket.py
+++ rocket/rocket.py
@@ -22,14 +22,32 @@ while y<625:
             if event.key==K_UP:
                 keys[0]=True
             if event.key==K_DOWN:
+                keys[1]=True
+            if event.key==K_LEFT:
                 keys[2]=True
+            if event.key==K_RIGHT:
+                keys[3]=True
         if event.type==pygame.KEYUP:
             if event.key==K_UP:
                 keys[0]=False
+            if event.key==K_DOWN:
+                keys[1]=False
+            if event.key==K_LEFT:
+                keys[2]=False
+            if event.key==K_RIGHT:
+                keys[3]=False
     if keys[0]:
         if y>0:
             y-=15
+    if keys[1]:
+        if y<610:
+            y+=15
+    if keys[2]:
+        if x>0:
+            x-=15
+    if keys[3]:
+        if x<940:
+            x+=15
     y+=10
     sleep(0.05)
     pygame.display.update()
-            
