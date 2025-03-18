8b95cf528a25d20ba144aed1ddc68090fdb9d506
erina-ameen <erina.ameen@gmail.com>
2025-03-18 15:27:02 +0000

    Add files via upload

---
+++ Ninjas/ninjas.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import*
+
+pygame.font.init()
+WIDTH=736
+HEIGHT=558
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Ninja Fighters")    #title for the window
+white=(255, 255, 255)
+black=(0, 0, 0)
+purple=(201, 140, 209)
+blue=(140, 176, 209)
