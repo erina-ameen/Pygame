696a2ec5f159d08669dde5f7c21ed14c33a08efa
erina-ameen <erina.ameen@gmail.com>
2025-03-25 15:26:38 +0000

    ninjas.py

--- Ninjas/ninjas.py
+++ Ninjas/ninjas.py
@@ -2,11 +2,52 @@ import pygame
 from pygame.locals import*
 
 pygame.font.init()
-WIDTH=736
-HEIGHT=558
+pygame.mixer.init()
+WIDTH=1000
+HEIGHT=759
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Ninja Fighters")    #title for the window
 white=(255, 255, 255)
 black=(0, 0, 0)
 purple=(201, 140, 209)
 blue=(140, 176, 209)
+font1=pygame.font.SysFont("Georgia", 30)
+font2=pygame.font.SysFont("Times New Roman", 70)
+
+#Variables
+framespersecond=60
+velocity=7
+bulletspeed=9
+maxbullet=5
+
+#Images
+bg=pygame.image.load("ninjahouse.jpg")
+n1=pygame.image.load("ninja1.png")
+n2=pygame.image.load("ninja2.png")
+
+#Border
+border=pygame.Rect(500,0,5,759)
+nhealth1=10
+nhealth2=10
+
+#Class for ninja
+def ninja(pygame.sprite.Sprite):
+    def __init__(self,image,x,y):
+        super().__init__()
+        self.image=pygame.image.load(image)
+def drawing():
+    screen.blit(bg,(0,0))
+    pygame.draw.rect(screen,black,border)
+    heathtxt1=font1.render("Health 1: "+str(nhealth1),1,white)
+    heathtxt2=font1.render("Health 2: "+str(nhealth2),1,white)
+    screen.blit(heathtxt1, (0,0))
+    screen.blit(heathtxt2, (800,0))
+run=True
+while run:
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            run=False
+        drawing()    
+    pygame.display.update()
+
+pygame.quit()
