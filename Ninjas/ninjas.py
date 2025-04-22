a1e63edfacac6a89a26d9aa22841c377b04b0d1f
erina-ameen <erina.ameen@gmail.com>
2025-04-22 15:01:57 +0100

    ninjas.py

--- Ninjas/ninjas.py
+++ Ninjas/ninjas.py
@@ -38,7 +38,8 @@ class ninja(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.x=x
         self.rect.y=y
-    def UP(self,speed):
+
+    def UP_DOWN(self,speed):
         self.rect.move_ip(0,speed)
         if self.rect.top<=0 or self.rect.bottom>=759:
             self.rect.move_ip(0,-speed)
@@ -47,6 +48,10 @@ class ninja(pygame.sprite.Sprite):
         self.rect.move_ip(speed,0)
         if player==1:
             if self.rect.left<=0 or self.rect.right>=border.left:
+                self.rect.move_ip(-speed,0)
+
+        if player==2:
+            if self.rect.right>=1000 or self.rect.left<=border.right:
                 self.rect.move_ip(-speed,0)
 
 def drawing():
@@ -69,6 +74,28 @@ while run:
     for event in pygame.event.get():
         if event.type==QUIT:
             run=False
+    press_key=pygame.key.get_pressed()
+
+    #right ninja movement
+    if press_key[K_LEFT]:
+        rightninja.LEFT_RIGHT(-velocity,2)
+    if press_key[K_RIGHT]:
+        rightninja.LEFT_RIGHT(velocity,2)
+    if press_key[K_UP]:
+        rightninja.UP_DOWN(-velocity)
+    if press_key[K_DOWN]:
+        rightninja.UP_DOWN(velocity)
+
+    #left ninja movement
+    if press_key[K_a]:
+        leftninja.LEFT_RIGHT(-velocity,1)
+    if press_key[K_d]:
+        leftninja.LEFT_RIGHT(velocity,1)
+    if press_key[K_w]:
+        leftninja.UP_DOWN(-velocity)
+    if press_key[K_s]:
+        leftninja.UP_DOWN(velocity)
+
     drawing()    
     ninjagroup.draw(screen)
     pygame.display.update()
